f7d6379dc4061a95d61d3be02d3595a77b4577c1
Sebastian-OConnell <160685205+Sebastian-OConnell@users.noreply.github.com>
2024-04-08 15:30:30 +1000

    Create part_d_main.c
    Need to add comments

---
+++ Exercise 2/part_d_main.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "first_serial.h"
+#include "stm32f303xc.h"
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+void finished_transmission(uint32_t bytes_sent) {
+	for (volatile uint32_t i = 0; i < 0x8; i++) {
+	}
+}
+
+void received_new_string(uint8_t *buffer, uint32_t bytes_sent) {
+    buffer[bytes_sent] = '\r';  // Carriage return
+    buffer[bytes_sent + 1] = '\n';  // Newline
+    buffer[bytes_sent + 2] = '\0';  // Null-terminator
+	SerialOutputString(buffer, &USART1_PORT);
+}
+
+int main(void) {
+    SerialInitialise(BAUD_115200, &USART1_PORT, finished_transmission, received_new_string);
+    //run_serial_test();
+    for (;;) {
+    }
+}
