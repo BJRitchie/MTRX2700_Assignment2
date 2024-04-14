1bcf1578d9710577934d644c42a9765bf4c9cc9a
Sebastian-OConnell <160685205+Sebastian-OConnell@users.noreply.github.com>
2024-04-14 17:11:54 +1000

    Update part_c_main.c
    comments

--- Exercise 2/part_c_main.c
+++ Exercise 2/part_c_main.c
@@ -2,26 +2,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Include the relevant header files
 #include "serial.h"
 #include "stm32f303xc.h"
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
-
+// Callback function to simulate a delay after transmission
 void finished_transmission(uint32_t bytes_sent) {
 	for (volatile uint32_t i = 0; i < 0x8; i++) {
 	}
 }
 
+// Callback function that echoes the received string back to the sender
 void received_new_string(uint8_t *buffer, uint32_t bytes_sent) {
 	SerialOutputString(buffer, &USART1_PORT);
 }
 
 int main(void) {
+	
+// Initialising the serial module
     SerialInitialise(BAUD_115200, &USART1_PORT, finished_transmission, received_new_string);
-    //run_serial_test();
+	
+// Loop indefinitely 
     for (;;) {
-
     }
 }
