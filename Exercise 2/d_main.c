6e496f39ebcae1aed5bbf2d11c616288bdcd8371
Sebastian-OConnell <160685205+Sebastian-OConnell@users.noreply.github.com>
2024-04-14 17:29:33 +1000

    Update d_main.c

--- Exercise 2/d_main.c
+++ Exercise 2/d_main.c
@@ -4,27 +4,35 @@
 #include <string.h>
 
 // Include relevant header files
-#include "serial.h"
+#include "d_serial.h"
 #include "stm32f303xc.h"
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
-
+// Callback function to simulate a delay after transmission
 void finished_transmission(uint32_t bytes_sent) {
+	
+// This loop simulates a short delay
 	for (volatile uint32_t i = 0; i < 0x8; i++) {
 	}
 }
 
+// Callback function that is triggered when a new string is received
 void received_new_string(uint8_t *buffer, uint32_t bytes_sent) {
-    buffer[bytes_sent] = '\r';  // Carriage return
-    buffer[bytes_sent + 1] = '\n';  // Newline
-    buffer[bytes_sent + 2] = '\0';  // Null-terminator
+	
+// Add a carriage return and newline at the end of the string to format the output
+    	buffer[bytes_sent] = '\r';  // Carriage return to return the cursor to the beginning of the line
+    	buffer[bytes_sent + 1] = '\n';  // New line to advance to the next line
+    	buffer[bytes_sent + 2] = '\0';  // Null terminator to end the string
+	
+// Output the received string back to the serial port, useful for echo tests or command line interfaces
 	SerialOutputString(buffer, &USART1_PORT);
 }
 
 int main(void) {
+
+// Initialising the serial module
     SerialInitialise(BAUD_115200, &USART1_PORT, finished_transmission, received_new_string);
+	
+// Loop indefinitely
     for (;;) {
     }
 }
