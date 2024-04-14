97cde4edc7d5353d41b0594a6427a4da79cd6682
Sebastian-OConnell <160685205+Sebastian-OConnell@users.noreply.github.com>
2024-04-14 17:07:07 +1000

    Update serial.c

--- Exercise 2/serial.c
+++ Exercise 2/serial.c
@@ -4,6 +4,7 @@
 static uint8_t received_string[64]; // Buffer for received string
 static uint32_t received_length = 0; // Length of the received string
 
+// Serial port structure definition, including GPIO and USART settings
 struct _SerialPort {
 	USART_TypeDef *UART;
 	GPIO_TypeDef *GPIO;
@@ -18,6 +19,7 @@ struct _SerialPort {
 	SerialReceiveCallback receive_callback;
 };
 
+// Configuration for USART1
 SerialPort USART1_PORT = {USART1,
 		GPIOC,
 		RCC_APB2ENR_USART1EN, // bit to enable for APB2 bus
@@ -30,7 +32,7 @@ SerialPort USART1_PORT = {USART1,
 		0x00 // default function pointer is NULL
 		};
 
-
+// Function to read input string from serial until a terminating character is read
 uint32_t SerialInputString(uint8_t *buffer, uint32_t buffer_size, SerialPort *serial_port) {
     static uint8_t last_character = 0;  // Static variable to store the last character received
     uint32_t count = 0;
@@ -57,7 +59,7 @@ uint32_t SerialInputString(uint8_t *buffer, uint32_t buffer_size, SerialPort *se
     return count;
 }
 
-
+// Test function to repeatedly receive and echo strings
 void run_serial_test(void) {
     //uint8_t *string_to_send = "STM32 ready to receive:\r\n";
     uint8_t received_string[64]; // Buffer for received string
@@ -69,8 +71,7 @@ void run_serial_test(void) {
     }
 }
 
-// InitialiseSerial - Initialise the serial port
-// Input: baudRate is from an enumerated set
+// Initialise the serial port with specified baud rate and callback functions
 void SerialInitialise(uint32_t baudRate, SerialPort *serial_port, void (*completion_function)(uint32_t), SerialReceiveCallback receive_callback) {
 
 	serial_port->completion_function = completion_function;
@@ -97,24 +98,20 @@ void SerialInitialise(uint32_t baudRate, SerialPort *serial_port, void (*complet
 	RCC->APB2ENR |= serial_port->MaskAPB2ENR;
 
 	// Get a pointer to the 16 bits of the BRR register that we want to change
-	uint16_t *baud_rate_config = (uint16_t*)&serial_port->UART->BRR; // only 16 bits used!
+	uint16_t *baud_rate_config = (uint16_t*)&serial_port->UART->BRR; // only 16 bits used
 
 	// Baud rate calculation from datasheet
 	switch(baudRate){
 	case BAUD_9600:
-		// NEED TO FIX THIS !
 		*baud_rate_config = 0x46;  // 115200 at 8MHz
 		break;
 	case BAUD_19200:
-		// NEED TO FIX THIS !
 		*baud_rate_config = 0x46;  // 115200 at 8MHz
 		break;
 	case BAUD_38400:
-		// NEED TO FIX THIS !
 		*baud_rate_config = 0x46;  // 115200 at 8MHz
 		break;
 	case BAUD_57600:
-		// NEED TO FIX THIS !
 		*baud_rate_config = 0x46;  // 115200 at 8MHz
 		break;
 	case BAUD_115200:
@@ -129,50 +126,57 @@ void SerialInitialise(uint32_t baudRate, SerialPort *serial_port, void (*complet
 	serial_port->receive_callback = receive_callback;
 }
 
+// Enables the receive interrupt for the USART
 void SerialEnableInterrupt(SerialPort *serial_port) {
     // Enable the USART receive interrupt
     serial_port->UART->CR1 |= USART_CR1_RXNEIE;
 
     // Enable the USART interrupt in the NVIC
-    NVIC_SetPriority(USART1_IRQn, 1);
-    NVIC_EnableIRQ(USART1_IRQn);
-
-
+    NVIC_SetPriority(USART1_IRQn, 1); // Set priority level to 1
+    NVIC_EnableIRQ(USART1_IRQn); // Enable IRQ for USART1
 }
 
+// Function to send a single character over serial
 void SerialOutputChar(uint8_t data, SerialPort *serial_port) {
-
+	// Wait for the transmit data register to be empty
 	while((serial_port->UART->ISR & USART_ISR_TXE) == 0){
 	}
 
+	// Load the data into the transmit data register
 	serial_port->UART->TDR = data;
 }
 
+// Function to send a string of characters over serial
 void SerialOutputString(uint8_t *pt, SerialPort *serial_port) {
-
-	uint32_t counter = 0;
-	while(*pt) {
-		SerialOutputChar(*pt, serial_port);
+	uint32_t counter = 0; // Counter for the number of bytes sent
+	while(*pt) { // Loop until end of string
+		SerialOutputChar(*pt, serial_port); // Send current character and increase counter
 		counter++;
 		pt++;
 	}
-
+	
+	// Call the completion function after the whole string has been sent
 	serial_port->completion_function(counter);
 }
 
+// Interrupt handler for USART1
 void USART1_IRQHandler(void) {
-    static uint8_t received_buffer[64];
-    static uint32_t received_length = 0;
+    static uint8_t received_buffer[64]; // Buffer to store received data
+    static uint32_t received_length = 0; // Length of received data
 
+    // Check if the receive data register is not empty
     if (USART1->ISR & USART_ISR_RXNE) {
         uint8_t received_byte = USART1->RDR; // Read the received byte
 
-        if (received_byte == '\r') { // Check for the carriage return character
+	// Check if the received byte is the carriage return character
+        if (received_byte == '\r') { 
+	    // Call the receive callback function if it is not NULL
             if (USART1_PORT.receive_callback != NULL) {
                 USART1_PORT.receive_callback(received_buffer, received_length); // Trigger the callback
             }
             received_length = 0; // Reset the buffer for the next message
         } else {
+	    // Check if there is space in the buffer
             if (received_length < sizeof(received_buffer) - 1) {
                 received_buffer[received_length++] = received_byte; // Store the byte in the buffer
                 received_buffer[received_length] = '\0'; // Null-terminate the string
