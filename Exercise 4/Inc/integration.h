02253a4b52c01cdd5ee27edc1a4004f7ff10d5aa
Sebastian-OConnell <160685205+Sebastian-OConnell@users.noreply.github.com>
2024-04-16 15:39:48 +1000

    Update integration.h

--- Exercise 4/Inc/integration.h
+++ Exercise 4/Inc/integration.h
@@ -11,8 +11,10 @@
 #include <serial.h>
 #include <timer.h>
 
+// Function for receiving string sent consisting of a command and parameters
 void received_new_string(uint8_t *buffer, uint32_t length);
 
+// Carry out a function based on the command inputted
 void execute_command(const char* command, const char* params);
 
 #endif
